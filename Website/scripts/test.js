4b39a62d88f598478bf4ca48dd23f059022576ca
Olia <penguin.tut@gmail.com>
2011-09-28 00:16:01 +0100

    Added gallery with flickr photo stream
    I used http://galleria.aino.se/ Flickr plugin.

--- Website/scripts/test.js
+++ Website/scripts/test.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
 	$('#header').localScroll();
+	
+	var flickr = new Galleria.Flickr();
+	flickr.user('67870114@N05', function(data) {
+	    $('#gallery').galleria({
+	        dataSource: data
+	    });
+	});
+	
 });
+
+
